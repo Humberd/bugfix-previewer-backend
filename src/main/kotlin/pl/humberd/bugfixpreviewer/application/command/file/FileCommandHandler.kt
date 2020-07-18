beb289c377fe2e9a7e2367b594a5646b7f6cfff0
humberd <humberd2@gmail.com>
2020-07-18 15:22:36 +0200

    Temporarily mocked aws uploader

--- src/main/kotlin/pl/humberd/bugfixpreviewer/application/command/file/FileCommandHandler.kt
+++ src/main/kotlin/pl/humberd/bugfixpreviewer/application/command/file/FileCommandHandler.kt
@@ -1,6 +1,5 @@
 package pl.humberd.bugfixpreviewer.application.command.file
 
-import com.amazonaws.services.s3.AmazonS3
 import org.springframework.stereotype.Service
 import pl.humberd.bugfixpreviewer.application.command.file.model.FileUploadCommand
 import pl.humberd.bugfixpreviewer.application.command.file.model.FileUploadResult
@@ -13,7 +12,8 @@ class FileCommandHandler(
 
     fun upload(command: FileUploadCommand): FileUploadResult {
         return FileUploadResult(
-            url = awsS3Bridge.uploadFile(command.file)
+//            url = awsS3Bridge.uploadFile(command.file)
+            url = "https://testmobilehub-hosting-mobilehub-825908255.s3.eu-central-1.amazonaws.com/example-video-1.mp4"
         )
     }
 }
